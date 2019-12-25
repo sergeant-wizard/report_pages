9d38d26d02e0f5d37e7220a601d095b1bd5e947c
Ryo Miyajima <sergeant.wizard@gmail.com>
2019-12-24 22:51:07 -0800

    two analysis

---
+++ analysis1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot
+import numpy
+import pandas
+
+import my_module.hoge
+
+
+pandas.DataFrame(
+    my_module.hoge.get_results(),
+    index=['s1', 's2', 's3']
+).to_html('docs/_includes/table.html')
+
+x = numpy.linspace(-6, 6, 12)
+matplotlib.pyplot.plot(
+    x, numpy.sin(x))
+matplotlib.pyplot.savefig('docs/assets/chart.png')
